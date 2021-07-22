da7e066af4d3f1aea5c01d18420abb8c02d33e4b
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2021-07-22 12:13:05 +0100

    Added location shortname to resource namer
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- deploy/azure/app/csr/main.tf
+++ deploy/azure/app/csr/main.tf
@@ -2,7 +2,7 @@ module "default_label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.24.1"
   namespace  = "${var.name_company}-${var.name_project}"
   stage      = var.stage
-  name       = var.name_component
+  name       = "${lookup(var.location_name_map, var.resource_group_location, "northeurope")}-${var.name_component}"
   attributes = var.attributes
   delimiter  = "-"
   tags       = var.tags
